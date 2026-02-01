49065fb03d9062ab4f69074ad5076736566bfeaa
makhmudfoziljonov <mahmudjonfoziljonov1@gmail.com>
2026-02-01 22:29:24 +0500

    smth

---
+++ src/main/java/com/simplyportfolio/SimplyPortfolioApplication.java
@@ -0,0 +1,13 @@
+package com.simplyportfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplyPortfolioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimplyPortfolioApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/simplyportfolio/SimplyPortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplyportfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplyPortfolioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
